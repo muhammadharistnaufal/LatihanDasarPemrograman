d0e62a252117686d6a15aacf16d4a924de96a59a
muhammadharistnaufal <114551807+muhammadharistnaufal@users.noreply.github.com>
2022-09-29 11:17:48 +0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,24 @@
+# Aplikasi Bioddata Buatan Oleh M. Harist Naufal
+
+#Input Nama
+Nama = input("masukan nama anda!")
+# Print / output Nama
+print("Nama anda adalah : ",Nama)
+#Input NIM
+NIM = input("masukan NIM anda!")
+# Print / output NIM
+print("NIM anda adalah : ",NIM)
+#Input Kelas
+KELAS = input("masukan kelas anda!")
+# Print / output Kelas
+print("KELAS anda adalah : ",KELAS)
+#Input Prodi
+prodi = input("masukan prodi anda!")
+# Print / output Prodi
+print("Prodi anda adalah : ",prodi)
+#Input Tanggal Lahir
+Tanggallahir = input("masukan TTL anda!")
+# Print / output Tanggal Lahir
+print("Tanggal Lahir  anda adalah : ",Tanggallahir)
+
+
